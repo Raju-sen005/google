081e1d70e547b3064cb8eb72ac3a94ac61ed0fef
Raju_0005 <rajugbp2006@gmail.com>
2025-02-04 21:05:04 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+document.getElementsByTagName("h1")[0].style.color = "blue";
+document.getElementsByTagName("h2")[0].style.color = "red";
+document.getElementsByTagName("h3")[0].style.color = "yellow";
+document.getElementsByTagName("h4")[0].style.color = "blue";
+document.getElementsByTagName("h5")[0].style.color = "green";
+document.getElementsByTagName("h6")[0].style.color = "red";
+
